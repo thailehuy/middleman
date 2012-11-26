2f65753496fd9095b27449087be6325298629fb0
HuyT <huy.thai@eastagile.com>
2012-11-26 10:01:13 +0700

    Implement push notification mechanism

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var express = require('express'),
   io = require('socket.io').listen(server),
   stylus = require('stylus'),
   nib = require('nib'),
+  redis   = require('redis'),
+  publisherClient = redis.createClient(),
   routes = require('./routes');
 
 // Configuration
@@ -61,12 +63,50 @@ server.listen(port);
 var router = require("./router.js");
 router.createRoutes(app, routes)
 
-var status = "All is well.";
+// events
+app.get('/update-stream', function(req, res) {
+  // let request last as long as possible
+  req.socket.setTimeout(Infinity);
 
-io.sockets.on('connection', function (socket) {
-  io.sockets.emit('status', { status: status }); // note the use of io.sockets to emit but socket.on to listen
-  socket.on('reset', function (data) {
-    status = "War is imminent!";
-    io.sockets.emit('status', { status: status });
+  var messageCount = 0;
+  var subscriber = redis.createClient();
+
+  subscriber.subscribe("updates");
+
+  // In case we encounter an error...print it out to the console
+  subscriber.on("error", function(err) {
+    console.log("Redis Error: " + err);
+  });
+
+  // When we receive a message from the redis connection
+  subscriber.on("message", function(channel, message) {
+    messageCount++; // Increment our message count
+
+    res.write('id: ' + messageCount + '\n');
+    res.write("data: " + message + '\n\n'); // Note the extra newline
+  });
+
+  //send headers for event-stream connection
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  res.write('\n');
+
+  // The 'close' event is fired when a user closes their browser window.
+  // In that situation we want to make sure our redis channel subscription
+  // is properly shut down to prevent memory leaks...and incorrect subscriber
+  // counts to the channel.
+  req.on("close", function() {
+    subscriber.unsubscribe();
+    subscriber.quit();
   });
 });
+
+app.get('/fire/:event/:token', function(req, res) {
+  publisherClient.publish( 'updates', ('"' + req.params.event + '" page visited') );
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('All clients have received "' + req.params.event + '"');
+  res.end();
+})
